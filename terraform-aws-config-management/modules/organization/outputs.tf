90da919e56ca1c073059882a866e530299dea7c4
Sai Siddarth Ancha <saisiddarth.ancha21@gmail.com>
2026-04-23 10:47:20 -0600

    removing unwanted files

--- terraform-aws-config-management/modules/organization/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "conformance_pack_arns" {
-  description = "ARNs of the created organization conformance packs"
-  value       = { for k, v in aws_config_organization_conformance_pack.pack : k => v.arn }
-}
-
-output "conformance_pack_names" {
-  description = "Names of the created organization conformance packs"
-  value       = { for k, v in aws_config_organization_conformance_pack.pack : k => v.name }
-}
